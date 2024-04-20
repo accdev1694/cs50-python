5fb1f2bd9e318952766ec190e4fedc980ea3837e
unknown <diloch111@gmail.com>
2024-04-20 22:27:45 +0100

    oloche aboje

--- 02_string_methods.py
+++ 02_string_methods.py
@@ -10,16 +10,21 @@ print(f"{name}, your name has been stripped")
 # string.capitalize()
 name = name.capitalize()
 # returns an upper case character for the first character
-print(f"{name}, your name has been capitalized")
+print(f"{name}, your name has been capitalize()")
 
 # string.title
 name = name.title()
 # type in more than one name
-print(f"{name}, your name is title-cased")
+print(f"{name}, your name is title()")
 # returns first character of each string upper cased
 
 # Combine functions - string.strip().title()
 name = input("what is your name: ").strip().title()
-print(f"{name}, your name is stripped, and title cased")
+print(f"{name}, your name is strip(), and title()")
+
+# string.split()
+# the split function takes in an input of the position of split()
+name = name.split(" ")
+print(f"Welcome, {name[0]}, your name has been split()")
 
 
