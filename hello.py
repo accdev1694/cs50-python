9587a7238764e1921454c098e295b4074632566b
unknown <diloch111@gmail.com>
2024-04-20 14:55:45 +0100

    introduction to variables

---
+++ hello.py
@@ -0,0 +1,31 @@
+# write my first program
+print("hello, world")           
+# type python hello.py to run the code
+# prints "hello, world" to the console
+
+# python command is calling the python interpreter to run the program by
+# converting the command to computer language
+
+# FUNCTIONS
+# they are like actions or verbs that help us do something on the computer
+
+# ARGUEMENTS
+# string of instructions inside a function that predtermines its behaviour#
+
+# SIDE EFFECTS
+# what gets printed on the screen
+
+# BUGS
+# mistakes in a program
+
+# INPUT
+# the variable input has a return value that you then store in a variable and use in greeting
+name = input("what is your name? ")
+print("hello,", name)
+
+# VARIABLES
+# a reusable container to store values in a computer program
+# "name" above is a variable-name
+# "=" is an assignment operator - copies from right to left, whatever the users input is
+# whatever the return value of the input function is, gets assigned as the value of the variable
+# 
